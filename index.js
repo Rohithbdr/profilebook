efa9e02edb6b133fd1e878ded338d47bb18faa22
Rohithbdr <34566736+Rohithbdr@users.noreply.github.com>
2022-11-22 13:31:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let modal = document.querySelector('.modal');
 let close = document.querySelector('.close');
 let userInput = document.getElementById("email");
 let userPass = document.getElementById("emailPass");
+let eyeIcon = document.getElementById('eyeIcon');
 
 let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName"); 
@@ -24,15 +25,34 @@ let genderbtn = document.querySelectorAll('.gender');
 
 
 let logedFirst = document.getElementById("loged-name");
-let logedLast = document.getElementById("loged-Lname");
+let logedSecond = document.getElementById("loged-name2");
 let profIcon = document.getElementById("profile-icon");
 let renderedBg = document.getElementById('render-bg');
 let logout = document.getElementById("logout");
+let viewProf = document.getElementById("profile-tab");
+let coverName = document.getElementById("coverName");
+let cardItems = document.querySelectorAll(".card-para");
+
+
+
 
 let bDate = document.getElementById("day");
 let bMonth = document.getElementById("month");
 let bYear = document.getElementById("year");
 let birthdays = document.querySelectorAll('.birthday');
+
+
+
+let fbIcon = document.querySelector(".fb-icon");
+let homeCont = document.getElementById("homeCont");
+let shortCont = document.getElementById("shortCont");
+let homeBg = document.querySelectorAll(".home-bg");
+
+
+
+
+
+
 
 
 let userArray = [];
@@ -50,6 +70,15 @@ genderbtn.forEach((event) => {
          }
     }
 })
+
+
+
+cardItems.forEach((event) => {
+    event.onclick = () => {
+        renderedBg.classList.remove("showUsers");
+    }
+})
+
 
 
 
@@ -95,6 +124,21 @@ pass.addEventListener("blur", (event) => {
     }
 });
 
+
+
+
+eyeIcon.addEventListener("click" , (event) => {
+    if(userPass.type === "password") {
+        userPass.setAttribute("type" , "text");
+        eyeIcon.name = "eye-outline";
+    } else {
+        userPass.setAttribute("type" , "password");
+        eyeIcon.name = "eye-off-outline";
+    }
+})
+
+
+
 dob.addEventListener("change" , (event) => {
     if(event.target.value === "") {
         dob.style.borderColor = "red";
@@ -120,6 +164,8 @@ inputs.forEach(function(element) {
 
 register.onclick = () => {
     modal.style.display = "block";
+    userInput.value = "";
+    userPass.value = "";
 
     if(firstName.value === "") {
         firstName.style.borderColor = "#dddfe2";
@@ -196,6 +242,8 @@ save.onclick = function() {
        
 }
 
+let filteredUser;
+
 logined.onclick = function () {
     let storedUsername = localStorage.getItem("usernames");
     let parsedUser = JSON.parse(storedUsername);
@@ -205,16 +253,128 @@ logined.onclick = function () {
 
 
     let parsedUserArray = JSON.parse(localStorage.getItem("users"));
-    let filteredUser = parsedUserArray.filter((each) => each.email.includes(userInput.value));
+     filteredUser = parsedUserArray.filter((each) => each.email.includes(userInput.value));
+    
     if((userInput.value === filteredUserList[0]) && (userPass.value === filteredPass[0])){
         logedFirst.textContent = filteredUser[0].firstName + " " + filteredUser[0].lastName;
+        logedSecond.textContent = filteredUser[0].firstName + " " + filteredUser[0].lastName;
         alert("Login Succesful");
         document.getElementById("main").style.display = "none";
+        document.getElementById("profileCont").style.display = "none";
+        document.getElementById('main2-cont').style.display = "flex";
+        document.getElementById('main2-cont').classList.add("showMain2");
         document.getElementById('main-2').style.display = "block";
     } else {
     alert("UserName or Password is incorrect");
 }
 }
+
+fbIcon.onclick = () => {
+    
+
+    homeBg.forEach((event) => {
+        event.style.color = "black";
+        event.style.borderBottomColor = "transparent";
+
+        document.querySelector(".house").style.color = "#1877f2";
+        document.querySelector(".house").style.borderBottom = "3px solid #1877f2";
+        event.classList.add("activve");
+    })
+
+    homeCont.style.display = "flex";
+    shortCont.style.display = "none";
+
+    document.getElementById("profileCont").style.display = "none";
+    document.getElementById('main2-cont').style.display = "flex";
+    document.getElementById('main2-cont').classList.add("showMain2");
+    renderedBg.classList.remove("showUsers");
+}
+
+let house = document.querySelector(".home");
+
+house.onclick = () => {
+    document.getElementById("profileCont").style.display = "none";
+    document.getElementById('main2-cont').style.display = "flex";
+    document.getElementById('main2-cont').classList.add("showMain2");
+    renderedBg.classList.remove("showUsers");
+}
+
+
+
+
+homeBg.forEach(function(event) {
+    event.onclick = function(e)  {
+
+        homeBg.forEach(function(list) {
+            list.style.color = "black";
+            list.style.borderBottom = "2px solid transparent";
+            list.classList.remove("activve");
+        })
+      
+        event.style.color = "#1877f2";
+        event.style.borderBottom = "3px solid #1877f2";
+        event.classList.add("activve");
+        renderHome(e.target.id);
+    }   
+});
+
+function renderHome(tag) {
+    switch(tag) {
+        case "home" : 
+            homeCont.style.display = "flex";
+            shortCont.style.display = "none";
+            shortCont.classList.remove("marketBg");
+            break;
+
+        case "shorts" : 
+            homeCont.style.display = "none";
+            shortCont.style.display = "flex";
+            shortCont.classList.add("marketBg");
+            break;
+
+        case "market" :
+            homeCont.style.display = "none";
+            shortCont.style.display = "flex";
+            shortCont.classList.add("marketBg");
+            break;
+
+        case "group"  :
+            homeCont.style.display = "none";
+            shortCont.style.display = "flex";
+            shortCont.classList.add("marketBg");
+            break;
+
+        case "game"   :
+            homeCont.style.display = "none";
+            shortCont.style.display = "flex";
+            shortCont.classList.add("marketBg");
+        default: break;
+    }
+}
+
+
+
+
+viewProf.onclick = function() {
+
+    renderedBg.classList.remove("showUsers");
+
+    homeBg.forEach((event) => {
+        event.style.color = "black";
+        event.style.borderBottomColor = "transparent";
+        event.classList.remove("activve");
+    })
+
+
+    document.getElementById("main").style.display = "none";
+    document.getElementById('main2-cont').style.display = "none";
+    document.getElementById('main2-cont').classList.remove("showMain2");
+    document.getElementById("profileCont").style.display = "block";
+
+    coverName.textContent = filteredUser[0].firstName + " " + filteredUser[0].lastName;
+}
+
+
 
 profIcon.onclick = () => {
   renderedBg.classList.toggle("showUsers");
@@ -227,7 +387,7 @@ logout.onclick = function() {
     alert("You have successfully been logged out");
 }
 
-
+document.getElementById("profileCont").style.display = "none";
 document.getElementById('main-2').style.display = "none";
 
 let username = [];
@@ -247,7 +407,10 @@ function saved() {
 
 function login(event) {
     if (event.key === "Enter"){
-    console.log(userName.value)
+    console.log(userName.value);
+    console.log("hi")
+    } else {
+        console.log("no")
     }
 }
 
